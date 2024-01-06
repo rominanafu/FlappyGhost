5ea5d73af2f0b9a7e1a1d8d963f3ab949eb1d95d
rominanafu <romina.najera.fuentes@gmail.com>
2024-01-06 16:13:38 -0600

    Added Game Over message

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 // Freeze scene and show game over message
 public class GameOver : MonoBehaviour {
 
+    [SerializeField]
+    GameObject prefabGameOver;
+
     // Show message of game over
     void GameOverMessage() {
-        return;
+        // Set location of the message
+        Vector3 location = new Vector3(Screen.width/2, Screen.height/2, 0);
+        location = Camera.main.ScreenToWorldPoint(location);
+        location.y += 1f;
+        location.z = -1;
+
+        // Show message
+        GameObject gameover = Instantiate<GameObject>(prefabGameOver, location, Quaternion.identity);
     }
 
     // Called when the ghost collides
